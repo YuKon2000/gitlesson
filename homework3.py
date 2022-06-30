87845abf4c80884e6607ea9b37a341ca709f0a16
yu2000kon <yu2000kon@yahoo.com>
2022-06-30 21:57:47 +0300

    completed task 7-10

---
+++ homework3.py
@@ -0,0 +1,21 @@
+#my_dict:
+my_list=[42, 43]
+my_dict={
+    'foo':{'a':12,
+           'b':(1,2,3,4,my_list)
+           },
+    'bar':{
+        'c':12,
+        'd':{5,6,7,8}
+    },
+    'moo':{
+        'e':12,
+        'f':{'Lol': ['L','o','l']}
+    },
+}
+my_dict['moo']['f']['Lol'].remove('o')
+print(my_dict)
+my_dict['moo']['f']['K']='K','e','k'
+print(my_dict)
+my_dict.clear()
+print(my_dict)
